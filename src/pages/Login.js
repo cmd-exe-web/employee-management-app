887b52b1935600b36864acd492028d4815ea389b
Akash Verma <aakashverma7184@gmail.com>
2023-03-22 03:24:28 +0530

    Added Login functionality
    Added two way binding in login page, added client side error handling for if the email and passowrd are empty, handled the error handling for server-side error ApiResponse such as user with given email doesnt exist (404) and entered email or password invalid (400) for when password is wrong

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
 import {
   Button,
   Card,
@@ -12,8 +14,56 @@ import {
   Row,
 } from "reactstrap";
 import Base from "../components/Base";
+import { loginUser } from "../services/user-service";
 
 const Login = () => {
+  const [loginDetail, setLoginDetail] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event, field) => {
+    let actualValue = event.target.value;
+    setLoginDetail({
+      ...loginDetail,
+      [field]: actualValue,
+    });
+  };
+
+  const handleReset = () => {
+    setLoginDetail({
+      username: "",
+      password: "",
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(loginDetail);
+
+    //validation
+    if (loginDetail.username.trim() == "" || loginDetail.password == "") {
+      toast.error("Email and password are required!");
+      return;
+    }
+
+    //submit the login detail to server to generate token
+    loginUser(loginDetail)
+      .then((data) => {
+        console.log("user login: ");
+        console.log(data);
+        toast.success("Login successful");
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status == 404 || error.response.status == 400) {
+          toast.error(error.response.data.message);
+        } else {
+          toast.error("Something went wrong on server!!");
+        }
+      });
+  };
+
   return (
     <Base>
       <Container>
@@ -29,26 +79,42 @@ const Login = () => {
                 <h3>Login Here!</h3>
               </CardHeader>
               <CardBody>
-                <Form>
+                <Form onSubmit={handleFormSubmit}>
+                  {/* enter email field */}
                   <FormGroup>
                     <Label for="email">Enter email</Label>
                     <Input
                       type="text"
                       id="email"
                       placeholder="Enter email here"
+                      value={loginDetail.username}
+                      onChange={(e) => {
+                        handleChange(e, "username");
+                      }}
                     />
                   </FormGroup>
+
+                  {/* enter password field */}
                   <FormGroup>
                     <Label for="password">Enter password</Label>
                     <Input
                       type="password"
                       id="password"
                       placeholder="Enter password here"
+                      value={loginDetail.password}
+                      onChange={(e) => {
+                        handleChange(e, "password");
+                      }}
                     />
                   </FormGroup>
                   <Container className="text-center">
                     <Button color="primary">Login</Button>
-                    <Button type="reset" className="ms-2" color="danger">
+                    <Button
+                      onClick={handleReset}
+                      type="reset"
+                      className="ms-2"
+                      color="danger"
+                    >
                       Reset
                     </Button>
                   </Container>
